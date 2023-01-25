3354e7d6c95b2f4202f991932f86b3d8c65d0dff
Steven Opio <stevenviko@gmail.com>
2023-01-24 17:43:51 -0800

    Task 1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+class square for 0-square-py
+"""
+
+class Square:
+    """
+    class whit private attribute size
+    """
+    def __init__(self, size):
+
+        self.__size = size

527a53424a8c63cefea93f82a5df5a4b8ee129a7
Steven Opio <stevenviko@gmail.com>
2023-02-07 14:49:39 -0800

    Task 8. Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module 8-class_to_json.
+Returns the dictionary description with
+simple data structure (list, dictionary,
+string, integer and boolean)
+for JSON serialization of an object.
+"""
+
+
+def class_to_json(obj):
+    return obj.__dict__

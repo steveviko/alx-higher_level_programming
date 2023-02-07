16f21218577e219042d2d6a184656c81afb9380a
Steven Opio <stevenviko@gmail.com>
2023-02-07 13:01:00 -0800

    Task 1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,14 +4,13 @@
 
 
 def write_file(filename="", text=""):
-    """ 
-    Function that writes to a text file
-        Args:
-               filename: filename
-               text: text to write
-        Raises
-               Exception: when the file can be opened
+    """ Function that writes to a text file
+    Args:
+        filename: filename
+        text: text to write
+    Raises
+        Exception: when the file can be opened
     """
 
     with open(filename, 'w', encoding="utf-8") as f:
-       return f.write(text)
+        return f.write(text)

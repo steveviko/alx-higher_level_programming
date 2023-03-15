5928d92ecf4e49056e63a31396ad2757aed73f16
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 <stevenviko@gmail.com>
2023-03-15 10:14:22 -0700

    Task 6. States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,4 @@
+-- Script that creates a database and a table
 CREATE DATABASE -- Query to create database hbtn_0d_usa
 IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states ( -- Query to create states table with 2 constraints

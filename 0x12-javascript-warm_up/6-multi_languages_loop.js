458e17145e253abe2ff388992760ed99cc1e5a05
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 <stevenviko@gmail.com>
2023-04-13 07:52:16 -0700

    Task 6. Loop to languages

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// script that prints 3 lines
+//Using an array of string and a loop
+
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let i = 0;
+
+while (i < array.length) {
+  console.log(array[i]);
+  i++;
+}

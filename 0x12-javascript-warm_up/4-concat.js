f6da1d8a8c2eb7d301fe32f3ec33b53c2ba14078
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 <stevenviko@gmail.com>
2023-04-12 21:09:05 -0700

    Task 4. Create a sentence

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// script that prints two arguments passed to it,
+//shown  in the format: “ is ”
+
+if (typeof process.argv[2] !== 'undefined' && typeof process.argv[3] !== 'undefined') {
+  console.log(process.argv[2] + ' is ' + process.argv[3]);
+} else if (typeof process.argv[3] === 'undefined') {
+  console.log(process.argv[2] + ' is undefined');
+} else {
+  console.log('undefined is undefined');
+}

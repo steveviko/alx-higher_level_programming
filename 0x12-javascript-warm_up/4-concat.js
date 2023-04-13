da1064ad075855f0d0083d3cb1a31ca87e658a01
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 <stevenviko@gmail.com>
2023-04-13 07:43:56 -0700

    Task 4 update

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-// script that prints two arguments passed to it,
-//shown  in the format: “ is ”
+// script that prints two arguments passed to it, shown in the format: “ is ”
 
 if (typeof process.argv[2] !== 'undefined' && typeof process.argv[3] !== 'undefined') {
   console.log(process.argv[2] + ' is ' + process.argv[3]);
